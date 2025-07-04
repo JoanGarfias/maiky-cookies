6d45a15cbd69470c96fa026cacddbf7b9c7379e7
Joan <113860964+JoanGarfias@users.noreply.github.com>
2025-07-03 23:58:12 -0600

    some changes in category main component

--- src/components/CardMain.vue
+++ src/components/CardMain.vue
@@ -11,52 +11,67 @@
 
 <template>
     <div class="card">
-        <h3 class="card-name">{{ nombre }}</h3>
         <img :src="category.img" alt="Imagen de categoría" />
-        <button type="button"><RouterLink :to="category.url">Ver más</RouterLink></button>
+        <div class="card-options">
+          <h3 class="card-name">{{ nombre }}</h3>
+          <RouterLink :to="category.url" title="Ver catálogo">
+            <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#e3e3e3"><path d="m298-262-56-56 121-122H80v-80h283L242-642l56-56 218 218-218 218Zm222-18v-80h360v80H520Zm0-320v-80h360v80H520Zm120 160v-80h240v80H640Z"/></svg>
+          </RouterLink>
+        </div>
     </div>
 </template>
 
 <style scoped>
     .card{
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      background-color: white;
+      border: 1px solid #ffffff;
+      box-shadow: 0 2px 8px rgba(37, 36, 36, 0.2);
+      border-radius: 20px;
+      transition: transform 0.5s ease;
     }
 
     .card h3{
-        text-align: center;
+      font-family: "Inter", sans-serif;
+      font-weight: 500;
+      text-align: center;
     }
 
     .card img{
         display: flex;
         justify-content: center;
         margin: 0 auto;
-        max-width: 280px;
-        max-height: 250px;
-        border-radius: 20px;
-        transition: transform 0.5s ease;
+        max-width: 210px;
+        max-height: 210px;
+        border-radius: 20px 20px 0px 0px;
     }
 
-    .card img:hover{
+    .card:hover{
         margin-bottom: 0.5rem;
         transform: scale(1.1);
     }
 
-    .card button{
-        all: unset;
-        display: flex;
-        justify-content: center;
-        text-align: center;
-        width: 50%;
-        margin: 0 auto;
-        border: 1px solid #3D4A87;
-        border-radius: 4px;
-        color: #3D4A87;
-        cursor: pointer;
+    .card-options{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      border-radius: 12px;
+      transition: bow-shadow 0.2s ease;
+      padding: 10px;
+
+      align-items: center;
     }
 
-    .card button a{
-        color: #3D4A87;
+    .card-options:hover{
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.12);
+    }
+
+    .card-options a{
+      color: #3D4A87;
+    }
+    .card-options svg{
+      fill: #3D4A87;
     }
 </style>
